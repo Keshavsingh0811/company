5cf22895a139d35e80481c3cf97265a8d7affbee
Keshavsingh0811 <keshavsingh39357@gmail.com>
2025-01-12 15:09:27 +0530

    bug in footer

--- UI/Footer/FooterOne.js
+++ UI/Footer/FooterOne.js
@@ -16,10 +16,6 @@ const FooterOne = (props) => {
           <p className="call-to-action-text">
             Every successful project starts with a conversation. Tell us your
             vision, and let's create a solution tailored to your needs.
-            Together, we’ll explore innovative approaches, overcome challenges,
-            and build something that not only meets but exceeds your
-            expectations. Let’s turn your ideas into reality and drive lasting
-            success.
           </p>
           <div className="call-to-action-button-wrapper">
             <button className="btn btn-warning" onClick={redirectToAnotherPage}>
